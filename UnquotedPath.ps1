c359e93357aa18327df3289b86b2ae7160a0cac0
d3hvck <106494573+d3hvck@users.noreply.github.com>
2024-11-29 15:52:57 +0400

    Update UnquotedPath.ps1

--- UnquotedPath.ps1
+++ UnquotedPath.ps1
@@ -14,7 +14,7 @@ USAGE: irm <github-raw-link> | iex
 $services = Get-WmiObject -Class Win32_Service
 foreach ($service in $services) {
     # Skip services where the PathName contains "C:\Windows\System32"
-    if ($service.PathName -and $service.PathName -notlike "C:\Windows\System32*") {
+    if ($service.PathName -and $service.PathName -notlike "C:\Windows\system32*") {
         # Check if the path is unquoted and contains spaces
         if ($service.PathName -match '^[^"]* [^"]*') {
             Write-Output "UnquotedSvc: $($service.Name) - $($service.PathName)"
