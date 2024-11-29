cdd51a6101b7b757bd4e82e968b00233294a1f9b
d3hvck <106494573+d3hvck@users.noreply.github.com>
2024-11-29 16:11:30 +0400

    Update UnquotedPath.ps1

--- UnquotedPath.ps1
+++ UnquotedPath.ps1
@@ -12,10 +12,10 @@ USAGE: irm <github-raw-link> | iex
 #>
 
 $services = Get-WmiObject -Class Win32_Service
-
 foreach ($service in $services) {
-    # Check for unquoted paths and ignore paths containing "C:\Windows\System32\svchost.exe"
-    if ($service.PathName -match '^[^"]* [^"]*' -and $service.PathName -notlike "C:\Windows\System32\svchost.exe*") {
-        Write-Output "Unquoted service path found: $($service.Name) - $($service.PathName)"
+    if ($service.PathName -match '^[^"]* [^"]*') {
+        if ($service.PathName -and $service.PathName -notlike "C:\Windows\system32\svchost.exe") {
+            Write-Output "UnquotedSvc: $($service.Name) - $($service.PathName)"
+        }
     }
 }
